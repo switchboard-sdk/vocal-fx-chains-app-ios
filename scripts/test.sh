d072d407f64e2d062c9038dfa57c477b85327514
Balázs Kiss <dev@balazskiss.name>
2025-07-28 11:57:23 +0200

    Updated ios version for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ set -eu
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 XCODE_PROJECT_PATH="${PROJECT_DIR}/VocalFXChainsApp.xcodeproj"
 SCHEME_NAME="VocalFXChainsApp"
-DESTINATION="platform=iOS Simulator,name=iPhone 15"
+DESTINATION="platform=iOS Simulator,name=iPhone 16"
 
 xcodebuild clean test \
   -project "$XCODE_PROJECT_PATH" \
